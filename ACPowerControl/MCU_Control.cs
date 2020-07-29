963b3ccbf2d4e05fb0715926b10d73497e7d09aa
FrankShepard <quhao_shepard@163.com>
2020-07-29 16:51:23 +0800

    Revert "调整IG-M3202F的进入校准命令的通讯代码"
    This reverts commit 8c52a24fb5a170fb1bd9aafe0fe122509ae66773.

--- ACPowerControl/MCU_Control.cs
+++ ACPowerControl/MCU_Control.cs
@@ -288,7 +288,7 @@ namespace ACPowerControl
 					strInformation = McuControl_vCommandSend( golden_finger , ref sp_mcu );
 					break;
 				case Product.SpeciesName.IG_M3202F: //泰和安通讯版本
-					golden_finger = new byte[] { 0xE6, 0x03, 0xDF, 0xD1, 0x5F };
+					golden_finger = new byte[] { 0xE6, 0x03, 0xDF, 0x5F,0xD1 };
 					strInformation = McuControl_vCommandSend( golden_finger, ref sp_mcu );
 					break;
 				case Product.SpeciesName.IG_X1101K://泰和安 需求10A箱式
